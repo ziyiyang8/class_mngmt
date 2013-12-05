7629bb3d51768ed1dfdd1fb291bc62fdea1ce559
Ziyi Yang <ziyiyang8@gmail.com>
2013-12-05 14:15:35 -0800

    redirect to student#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ClassMngmt::Application.routes.draw do
   resources :students
 
+  root to: 'students#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
